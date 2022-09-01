8a8015e79b097ef4819389d5fa6a84f752f40fe5
Tim McNamara <code@timmcnamara.co.nz>
2022-09-01 13:06:13 +1200

    Adding 01_01e

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-fn median(a: Vec<f32>) -> Option<f32> {
-    todo!();
+fn median(mut a: Vec<f32>) -> Option<f32> {
+    if a.is_empty() {
+        return None;
+    }
+
+    a.sort_by(|x, y| x.partial_cmp(y).unwrap());
+
+    let n_elements = a.len();
+    let middle = n_elements / 2;
+
+    let med = if n_elements % 2 == 0 {
+        (a[middle] + a[middle - 1]) / 2.0
+    } else {
+        a[middle]
+    };
+
+    Some(med)
 }
 
 fn main() {
